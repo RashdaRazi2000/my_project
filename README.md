407e36c7c2d8ec16bc7ec45c79296952c5d6ae29
Rashda Razi <rashdarazi@Rashdas-MacBook-Air.local>
2025-09-06 21:15:37 +0530

    chore: Initial commit with training script and requirements

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+# My Project
+
+This project is for practicing Git basics including branching, commits, and pushing to a remote repository.
+
 ## Features
 - Data cleaning script added.
---
+++ train.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import joblib
+
+#Load dataset
+iris = load_iris()
+X, y = iris.data, iris.target
+
+# Split into train/test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, 
+random_state=42)
+
+#Train model
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
+clf.fit(X_train, y_train)
+
+#Save Model
+joblib.dump(clf, "model.pkl")
+
+print("Model trained and saved as model.pkl")
+
+
